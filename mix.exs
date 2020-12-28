32156d8e4929d748b22837b15190d4430ec6cd5b
Derek Zhou <derek@3qin.us>
2020-12-28 18:28:58 -0500

    update deps

--- mix.exs
+++ mix.exs
@@ -10,7 +10,7 @@ defmodule Danm.MixProject do
       description: description(),
       package: package(),
       deps: deps(),
-
+      releases: [default: default_release()],
       # Docs
       name: "Danm",
       docs: [main: "readme",
@@ -28,7 +28,8 @@ defmodule Danm.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:html_writer, "~> 0.1.0"},
+      {:html_writer, "~> 0.1.1"},
+      # {:html_writer, path: "../html_writer"},
       {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
@@ -43,6 +44,12 @@ defmodule Danm.MixProject do
     [
       licenses: ["GPL-3.0-or-later"],
       links: %{"GitHub" => "https://github.com/derek-zhou/danm"}
+    ]
+  end
+
+  defp default_release do
+    [
+      {:include_erts, false},
     ]
   end
 
